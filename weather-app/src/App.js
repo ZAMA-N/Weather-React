ce256e0521dbc086bc576ac582ea66ef88a71e51
ZAMA-N <zamandlovuu@gmail.com>
2024-04-06 11:29:46 +0200

    added the Weather page

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Weather from './weather';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <h1>
+        <Weather/>
+      </h1>
         <p>
          welcome
         </p>
---
+++ weather-app/src/weather.js
@@ -0,0 +1,4 @@
+
+export default function Weather(){
+    return(hi)
+}
