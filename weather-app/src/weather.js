8042eb720691b0b71388546431925a7274c4a1d5
ZAMA-N <zamandlovuu@gmail.com>
2024-04-17 20:38:02 +0200

    fixing files

--- weather-app/src/weather.js
+++ weather-app/src/weather.js
@@ -4,7 +4,7 @@ import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
- export default function Weather(props) {
+export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
 
